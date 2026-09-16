0532b4de251060ef966158003b25317ad87265b4
Mohamed Anwar <mhmdwaelanwr@gmail.com>
2026-09-16 21:00:18 +0300

    Fix widget test provider setup

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -6,6 +6,7 @@ import 'package:shared_preferences/shared_preferences.dart';
 import 'package:markdown_creator/main.dart';
 import 'package:markdown_creator/providers/project_provider.dart';
 import 'package:markdown_creator/providers/library_provider.dart';
+import 'package:markdown_creator/services/subscription_service.dart';
 
 void main() {
   testWidgets('App loads and shows title', (WidgetTester tester) async {
@@ -14,11 +15,19 @@ void main() {
     tester.view.physicalSize = const Size(1200, 800);
     tester.view.devicePixelRatio = 1.0;
 
+    addTearDown(tester.view.resetPhysicalSize);
+    addTearDown(tester.view.resetDevicePixelRatio);
+
     await tester.pumpWidget(
       MultiProvider(
         providers: [
           ChangeNotifierProvider(create: (_) => ProjectProvider()),
-          ChangeNotifierProvider(create: (_) => LibraryProvider()),
+          ChangeNotifierProvider(
+            create: (_) => LibraryProvider(isFirebaseAvailable: false),
+          ),
+          ChangeNotifierProvider(
+            create: (_) => SubscriptionService(isFirebaseAvailable: false),
+          ),
         ],
         child: const MarkdownCreatorApp(),
       ),
@@ -28,8 +37,5 @@ void main() {
 
     expect(find.text('Markdown Creator'), findsOneWidget);
     expect(find.text('Components'), findsOneWidget);
-
-    addTearDown(tester.view.resetPhysicalSize);
-    addTearDown(tester.view.resetDevicePixelRatio);
   });
 }
